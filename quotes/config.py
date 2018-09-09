cba3ac7eb191a13eb783b3c1f7fec3b756703996
Pando85 <pando855@gmail.com>
2018-09-10 01:23:58 +0200

    Add flexibility to use envars to config

--- quotes/config.py
+++ quotes/config.py
@@ -1,5 +1,8 @@
+import os
 
-MONGO_USER = 'test'
-MONGO_PASSWORD = 'test1234'
-MONGO_URI = f'mongodb://{MONGO_USER}:{MONGO_PASSWORD}@localhost:27017'
-QUOTES_FILE_PATH = 'data/quotes.json'
+
+MONGO_USER = os.environ.get('MONGO_USER', 'test')
+MONGO_PASSWORD = os.environ.get('MONGO_PASSWORD', 'test1234')
+MONGO_URI = os.environ.get('MONGO_URI', f'mongodb://{MONGO_USER}:{MONGO_PASSWORD}@localhost:27017')
+
+QUOTES_FILE_PATH = os.environ.get('QUOTES_FILE_PATH', 'data/quotes.json')
