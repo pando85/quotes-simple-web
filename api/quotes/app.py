61ec10bfb7578b58f50bea4f43bef2edab95298d
Pando85 <pando855@gmail.com>
2018-10-24 18:18:16 +0200

    Refactor: change routes to list

--- api/quotes/app.py
+++ api/quotes/app.py
@@ -6,6 +6,8 @@ from quotes.handlers import author_handler, random_handler
 def get_app(setup_db):
     app = aiohttp.web.Application()
     app.on_startup.append(setup_db)
-    app.router.add_get('/quotes/random', random_handler)
-    app.router.add_get('/quotes/random/{author}', author_handler)
+    app.add_routes([
+        aiohttp.web.get('/quotes/random', random_handler),
+        aiohttp.web.get('/quotes/random/{author}', author_handler)
+    ])
     return app
