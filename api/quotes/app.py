06234db388a8f78b6de88deeb380ded7b504cd4e
pando85 <pando855@gmail.com>
2018-10-24 18:24:43 +0200

    Refactor: rename handlers (#4)
    * Refactor: rename handlers
    
    * Refactor: change routes to list

--- api/quotes/app.py
+++ api/quotes/app.py
@@ -1,11 +1,13 @@
 import aiohttp.web
 
-from quotes.routes import author_handler, random_handler
+from quotes.handlers import author_handler, random_handler
 
 
 def get_app(setup_db):
     app = aiohttp.web.Application()
     app.on_startup.append(setup_db)
-    app.router.add_get('/quotes/random', random_handler)
-    app.router.add_get('/quotes/random/{author}', author_handler)
+    app.add_routes([
+        aiohttp.web.get('/quotes/random', random_handler),
+        aiohttp.web.get('/quotes/random/{author}', author_handler)
+    ])
     return app
