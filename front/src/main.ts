72976e30730ca468869ad7ecc3ea8de631573e93
pando85 <pando855@gmail.com>
2018-10-31 17:41:31 +0100

    Add vue-touch with swipeleft support to get new quote

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import BootstrapVue from 'bootstrap-vue';
+import VueTouch from 'vue-touch';
 import App from './App.vue';
 import store from './store';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -7,6 +8,7 @@ import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
+Vue.use(VueTouch, {name: 'v-touch'});
 
 new Vue({
   store,
