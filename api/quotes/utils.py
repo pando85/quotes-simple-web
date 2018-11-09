d1d3d0c6b1d3119e432f2680404fb25af9d84fdf
Pando85 <pando855@gmail.com>
2018-11-09 17:47:58 +0100

    Add bind and compose functions

--- api/quotes/utils.py
+++ api/quotes/utils.py
@@ -1,16 +1,21 @@
-import pathlib
-from typing import List
+from typing import Any, TypeVar, Optional, Callable
 
 from quotes.quote import Quote
 
-
-def _list_files_in_directory(path: str) -> List[pathlib.Path]:
-    p = pathlib.Path(path).glob('**/*')
-    return [x for x in p if x.is_file()]
+T = TypeVar('T')
 
 
-def _create_file(path: str) -> None:
-    return open(path, 'a').close()
+def compose(*funcs: Any) -> Callable:
+    *rest, penultimate, last = funcs
+    if rest:
+        penultimate = compose(*rest, penultimate)
+    return lambda *args, **kwargs: penultimate(last(*args, **kwargs))
+
+
+def bind(f: Callable, x: Optional[T]) -> Optional[T]:
+    if x is None:
+        return None
+    return f(x)
 
 
 def dict_to_quote(_dict: dict) -> Quote:
