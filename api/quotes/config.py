de7596970a94f73cc57bf40bc2e09fce0585c0b1
Pando85 <pando855@gmail.com>
2018-11-10 19:02:38 +0100

    Change default quotes path

--- api/quotes/config.py
+++ api/quotes/config.py
@@ -6,7 +6,7 @@ MONGO_PASSWORD: str = os.environ.get('MONGO_PASSWORD', 'test1234')
 MONGO_URI: str = os.environ.get(
     'MONGO_URI', f'mongodb://{MONGO_USER}:{MONGO_PASSWORD}@localhost:27017')
 
-DATA_PATH: str = os.environ.get('DATA_PATH', 'data')
+DATA_PATH: str = os.environ.get('DATA_PATH', 'test/data')
 QUOTES_FILE_PATH: str = os.environ.get('QUOTES_FILE_PATH', f'{DATA_PATH}/quotes.json')
 
 CORS_ALLOW_ORIGIN: str = os.environ.get('CORS_ALLOW_ORIGIN', '*')
